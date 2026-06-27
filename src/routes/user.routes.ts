0a4605b207cb8f704bbf41e40eb5f5ebb0b6da4b
kumari.supreem <kumari.supreem@amantyatech.com>
2026-06-27 21:33:34 +0530

    user apis done

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -12,6 +12,6 @@ const router = Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/profile/updateProfile").post(authenticationToken, updateProfile);
-router.route("/logout").get(logout);
+router.route("/logout").post(logout);
 
 export default router;
