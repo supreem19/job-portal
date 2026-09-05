740ba365f7dc75a487accf9e6901681d6606ecec
kumari.supreem <kumari.supreem@amantyatech.com>
2026-09-05 21:00:03 +0530

    changes for cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5121"],
+  origin: ["http://localhost:3000"],
   credentials: true,
 };
 
